5cd742182bc0139cc241ebfcbcfdf691350244f7
siddltkfkd <unji2000@gmail.com>
2023-09-04 18:13:49 +0900

    곱셈기 만듬

--- Calculator.java
+++ Calculator.java
@@ -1,8 +1,10 @@
 public class Calculator {
     public static void main(String[] args) {
         Calc calc = new Calc();
+        Multiply mul = new Multiply();
 
         System.out.println(calc.plus(1, 1));
+        System.out.println(mul.multiply(2, 6));
         
     }
-}
+}듬
